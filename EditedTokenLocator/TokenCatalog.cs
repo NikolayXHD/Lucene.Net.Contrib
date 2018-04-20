df9d251ce10105cada41e2cc4e16e75de687faab
NikolayHD <hidalgo1984@list.ru>
2018-04-20 04:47:08 +0300

    formatting

--- EditedTokenLocator/TokenCatalog.cs
+++ EditedTokenLocator/TokenCatalog.cs
@@ -4,39 +4,40 @@ namespace Lucene.Net.Contrib
 {
 	public static class TokenCatalog
 	{
-		public static readonly Dictionary<string, TokenType> TypeByValue = new Dictionary<string, TokenType>
-		{
-			{ "AND", TokenType.And },
-			{ "&&", TokenType.And },
-			{ "+", TokenType.And },
+		public static readonly Dictionary<string, TokenType> TypeByValue =
+			new Dictionary<string, TokenType>
+			{
+				{ "AND", TokenType.And },
+				{ "&&", TokenType.And },
+				{ "+", TokenType.And },
 
-			{ "OR", TokenType.Or },
-			{ "||", TokenType.Or },
+				{ "OR", TokenType.Or },
+				{ "||", TokenType.Or },
 
-			{ "NOT", TokenType.Not },
-			{ "!", TokenType.Not },
-			{ "-", TokenType.Not },
+				{ "NOT", TokenType.Not },
+				{ "!", TokenType.Not },
+				{ "-", TokenType.Not },
 
-			{ "^", TokenType.BoostModifier },
-			{ "~", TokenType.SlopeModifier },
+				{ "^", TokenType.BoostModifier },
+				{ "~", TokenType.SlopeModifier },
 
-			{ "?", TokenType.AnyChar },
-			{ "*", TokenType.AnyString },
+				{ "?", TokenType.AnyChar },
+				{ "*", TokenType.AnyString },
 
-			{ "(", TokenType.OpenGroup },
-			{ "{", TokenType.OpenOpenRange },
-			{ "[", TokenType.OpenClosedRange },
+				{ "(", TokenType.OpenGroup },
+				{ "{", TokenType.OpenOpenRange },
+				{ "[", TokenType.OpenClosedRange },
 
-			{ ")", TokenType.CloseGroup },
-			{ "}", TokenType.CloseOpenRange },
-			{ "]", TokenType.CloseClosedRange },
+				{ ")", TokenType.CloseGroup },
+				{ "}", TokenType.CloseOpenRange },
+				{ "]", TokenType.CloseClosedRange },
 
-			{ "TO", TokenType.To },
-			{ ":", TokenType.Colon },
+				{ "TO", TokenType.To },
+				{ ":", TokenType.Colon },
 
-			{ "\"", TokenType.Quote },
-			{ "/", TokenType.RegexDelimiter }
-		};
+				{ "\"", TokenType.Quote },
+				{ "/", TokenType.RegexDelimiter }
+			};
 
 		public static TokenType? GetTokenType(string s)
 		{
